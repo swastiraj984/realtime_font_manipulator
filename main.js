010ca5903a8dd2feb04a841876214357e3663021
swastiraj984 <93324612+swastiraj984@users.noreply.github.com>
2023-04-07 15:56:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftWristX=0;
+rightWristX=0;
+
 function preload(){
 
 }
@@ -14,13 +20,24 @@ function modelLoaded(){
 }
 function draw(){
     background("#080808");
+    document.getElementById("text_size").innerHTML="Size of the text= "+difference+"px";
+    textSize(difference);
+    fill("#ffffff");
+    stroke("#ffffff");
+    text("Swasti",noseX,noseY);
 }
 function gotPoses(results){
-if(results>0){
+if(results.length>0){
     console.log(results);
     console.log("nosex="+results[0].pose.nose.x);
     console.log("nosey="+results[0].pose.nose.y);
     console.log("leftWristx="+results[0].pose.leftWrist.x);
     console.log("rightWristx="+results[0].pose.rightWrist.x);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    difference=floor(leftWristX-rightWristX);
+    console.log("difference= "+difference);
 }
 }
